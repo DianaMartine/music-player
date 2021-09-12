d9b2ff16ab97db1473a87b8691b8d6729a873430
Diana Martine <Diana_martine@outlook.com>
2021-09-12 13:21:14 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'Construção part1',
+        path: 'musics/construcao1.mp3',
+        artist: 'Diana Martine',
+        cover: 'images/cover.jpg'
+    },
+    {
+        name: 'Construção part2',
+        path: 'musics/construcao2.mp3',
+        artist: 'Diana Martine',
+        cover: 'images/cover.jpg'
+    }
+]
